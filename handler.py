ae03b01487b1b9b583b8f11248aec2e7567f47ff
Jock Reed <jock.da.rock@gmail.com>
2017-09-04 03:28:03 -0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -29,6 +29,20 @@ class ProxyFix(object):
         environ['SERVER_PROTOCOL'] = "HTTP/1.1"
         return self.app(environ, start_response)
 
+    
+ class HeaderRewriterFix(object):
+
+    def __init__(self, app, remove_headers=None, add_headers=None):
+        self.app = app
+        self.remove_headers = set(x.lower() for x in (remove_headers or ()))
+        self.add_headers = list(add_headers or ())
+
+    def __call__(self, environ, start_response):
+        def rewriting_start_response(status, headers, exc_info=None):
+            new_headers = []
+            return start_response(status, new_headers, exc_info)
+        return self.app(environ, rewriting_start_response)
+
 if __name__ == '__main__':
     app.wsgi_app = ProxyFix(app.wsgi_app)
     CGIHandler().run(app)
