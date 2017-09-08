f8ba6292c0d567f28a21bd6b598fd1e23d7def15
JockDaRock <jock.da.rock@gmail.com>
2017-09-08 14:00:24 -0500

    update

--- handler.py
+++ handler.py
@@ -2,11 +2,12 @@
 from wsgiref.handlers import CGIHandler
 from flask import render_template
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def hello(name=None):
     return render_template('hello.html', name=name)
 
@@ -16,6 +17,36 @@ def hello_again(name=None):
     return render_template('hello.html', name=name)
 
 
+class ProxyFix(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['SERVER_NAME'] = "localhost"
+        environ['SERVER_PORT'] = "8080"
+        environ['REQUEST_METHOD'] = "GET"
+        #environ['SCRIPT_NAME'] = ""
+        environ['PATH_INFO'] = "/"
+        #environ['QUERY_STRING'] = "name"
+        #environ['SERVER_PROTOCOL'] = "HTTP/1.1"
+        return self.app(environ, start_response)
+
+
+class HeaderRewriterFix(object):
+
+    def __init__(self, app, remove_headers=None, add_headers=None):
+        self.app = app
+        self.remove_headers = set(x.lower() for x in (remove_headers or ()))
+        self.add_headers = list(add_headers or ())
+
+    def __call__(self, environ, start_response):
+        def rewriting_start_response(status, headers, exc_info=None):
+            new_headers = []
+            return start_response(status, new_headers, exc_info)
+        return self.app(environ, rewriting_start_response)
+
 if __name__ == '__main__':
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app = ProxyFix(app)
+    #app.wsgi_app = ProxyFix(app.wsgi_app)
+    #app.wsgi_app = HeaderRewriteFix(app.wsgi_app)
     CGIHandler().run(app)
